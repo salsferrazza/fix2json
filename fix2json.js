750d05991e47de4a6bc72c2b1daa6ccf5349dbf4
Salvatore Sferrazza <sferrazza@google.com>
2018-06-02 14:10:30 -0400

    update identifier declarations to modern ES syntax

--- fix2json.js
+++ fix2json.js
@@ -1,26 +1,26 @@
 #! /usr/bin/env node
 
-var fs = require('fs');
-var zlib = require('zlib');
-var util = require('util');
-var xpath = require('xpath');
-var _ = require('underscore');
-var DOMParser = require('xmldom').DOMParser;
-var readline = require('readline');
-var StringDecoder = require('string_decoder').StringDecoder;
-var YAML = require('yamljs');
-var decoder = new StringDecoder();
-var delim = String.fromCharCode(01); // ASCII start-of-header
-var pretty = false;
-var dictname;
-var filename;
-var TAGS = {};
-var GROUPS = {};
-var MESSAGES = [];
-var FIX_VER = undefined;
-var rd = {};
-var yaml = false;
-var NUMERIC_TYPES = ['FLOAT', 'AMT', 'PRICE', 'QTY', 'INT', 'SEQNUM', 'NUMINGROUP', 'LENGTH', 'PRICEOFFSET'];
+const fs = require('fs');
+const zlib = require('zlib');
+const util = require('util');
+const xpath = require('xpath');
+const _ = require('underscore');
+const DOMParser = require('xmldom').DOMParser;
+const readline = require('readline');
+const StringDecoder = require('string_decoder').StringDecoder;
+const YAML = require('yamljs');
+const decoder = new StringDecoder();
+const delim = String.fromCharCode(01); // ASCII start-of-header
+let pretty = false;
+let dictname;
+let filename;
+let TAGS = {};
+let GROUPS = {};
+let MESSAGES = [];
+let FIX_VER = undefined;
+let rd = {};
+let yaml = false;
+const NUMERIC_TYPES = ['FLOAT', 'AMT', 'PRICE', 'QTY', 'INT', 'SEQNUM', 'NUMINGROUP', 'LENGTH', 'PRICEOFFSET'];
 
 // some of these TODO's below are very speculative:
 //
@@ -59,7 +59,7 @@ try {
 
     rd.on('line', function(line) {
         if (line.indexOf(delim) > -1) {
-            var msg = decoder.write(processLine(line));
+            const msg = decoder.write(processLine(line));
             console.log(msg);
         }
     });
@@ -71,11 +71,11 @@ try {
 
 function pluckGroup(tagArray, groupName) {
 
-	  var groupAnchor;
-    var group = [];
-    var member = {};
-    var firstProp = undefined;
-    var idx = 0;
+	  let groupAnchor;
+    let group = [];
+    let member = {};
+    let firstProp = undefined;
+    let idx = 0;
     
     if (tagArray && tagArray.length > 0) {
         groupAnchor = tagArray[0].tag;
@@ -83,13 +83,13 @@ function pluckGroup(tagArray, groupName) {
 
     while (tagArray.length > 0) {
 
-		    var tag = tagArray.shift();
-        var key = tag.tag;
-        var val = tag.val;
-        var num = tag.num;
+		    const tag = tagArray.shift();
+        let key = tag.tag;
+        let val = tag.val;
+        let num = tag.num;
 
-        var tagInGroup = _.contains(GROUPS[groupName], key);
-		    var type;
+        const tagInGroup = _.contains(GROUPS[groupName], key);
+		    let type;
 		    
 		    if (TAGS[num]) {
 	    	    type = TAGS[num].type ? TAGS[num].type : 'STRING';
@@ -104,7 +104,7 @@ function pluckGroup(tagArray, groupName) {
         } else if (type === 'NUMINGROUP') { // recurse into new repeating group
             member[key] = val;
             if (val > 0) {
-                var newGroup = pluckGroup(tagArray, key);
+                const newGroup = pluckGroup(tagArray, key);
                 member[key.substring('No'.length)] = newGroup.group;
                 tagArray = newGroup.fieldsLeft;
             }
@@ -127,7 +127,7 @@ function pluckGroup(tagArray, groupName) {
 function resolveFields(fieldArray) {
 
     targetObj = {};
-    var group = [];
+    let group = [];
 
     while (fieldArray.length > 0) {
 
@@ -157,7 +157,7 @@ function resolveFields(fieldArray) {
 }
 
 function processLine(line) {
-    var targetObj = resolveFields(extractFields(line));
+    let targetObj = resolveFields(extractFields(line));
     if (yaml) {
         return YAML.stringify(targetObj, 256);
     } else {
@@ -167,13 +167,13 @@ function processLine(line) {
 
 function extractFields(record) {
 
-    var fieldArray = [];
-    var fields = record.split(delim);
-    for (var i = 0; i < fields.length; i++) {
-        var both = fields[i].split('=');
+    let fieldArray = [];
+    let fields = record.split(delim);
+    for (let i = 0; i < fields.length; i++) {
+        let both = fields[i].split('=');
         both[0].replace("\n", '').replace("\r", '');
         if (both[1] !== undefined && both[0] !== undefined) {
-            var val = both[1];
+            let val = both[1];
             if (TAGS[both[0]] && TAGS[both[0]].type) {
                 val = _.contains(NUMERIC_TYPES, TAGS[both[0]].type) ? Number(val) : val;
             }
@@ -186,7 +186,6 @@ function extractFields(record) {
             });
         }
     }
-
     return fieldArray;
 }
 
@@ -195,24 +194,22 @@ function mnemonify(tag, val) {
 }
 
 function flattenComponent(componentName, dom) {
-    var fieldNames = [];
-    var components = xpath.select('//fix/components/component', dom);
+    let fieldNames = [];
+    const components = xpath.select('//fix/components/component', dom);
 
     if (!components || components.length === 0) {
         console.error('could not find component: ' + componentName);
         return fieldNames;
     } else {
-        for (var i = 0; i < components.length; i++) {
-            var fields = components[i].getElementsByTagName('field');
-            for (var j = 0; j < fields.length; j++) {
+        for (let i = 0; i < components.length; i++) {
+            const fields = components[i].getElementsByTagName('field');
+            for (let j = 0; j < fields.length; j++) {
                 fieldNames.push(fields[j].attributes[0].value);
             }
-
-            var comps = components[i].getElementsByTagName('component');
-            for (var k = 0; k < comps.length; k++) {
-                var compName = comps[k].attributes[0].value;
+            const comps = components[i].getElementsByTagName('component');
+            for (let k = 0; k < comps.length; k++) {
+                const compName = comps[k].attributes[0].value;
             }
-            
         }
         return _.uniq(fieldNames);
     }
@@ -223,27 +220,27 @@ function dictionaryGroups(dom) {
     let components = xpath.select('//fix/components/component', dom);
     let componentGroupFields = {};
 
-    for (var j = 0; j < components.length; j++) {
+    for (let j = 0; j < components.length; j++) {
 
-        var componentName = components[j].attributes[0].value;
+        const componentName = components[j].attributes[0].value;
         componentGroupFields[componentName] = {};
-        var componentGroups = components[j].getElementsByTagName('group');
+        const componentGroups = components[j].getElementsByTagName('group');
         
-        for (var k = 0; k < componentGroups.length; k++) {
+        for (let k = 0; k < componentGroups.length; k++) {
             const componentGroupName = componentGroups[k].attributes[0].value;
             GROUPS[componentGroupName] = [];
             componentGroupFields[componentName][componentGroupName] = [];
             const groupFields = componentGroups[k].getElementsByTagName('field');
 
-            for (var l = 0; l < groupFields.length; l++) {
+            for (let l = 0; l < groupFields.length; l++) {
                 const fieldName = groupFields[l].attributes[0].value;
                 GROUPS[componentGroupName].push(fieldName);
                 componentGroupFields[componentName][componentGroupName].push(fieldName);
             }
 
-            var groupComponents = componentGroups[k].getElementsByTagName('component');
+            const groupComponents = componentGroups[k].getElementsByTagName('component');
             for (l = 0; l < groupComponents.length; l++) {
-                var compName = groupComponents[l].attributes[0].value;
+                const compName = groupComponents[l].attributes[0].value;
                 GROUPS[componentGroupName].concat(flattenComponent(compName, dom));
                 componentGroupFields[componentName][componentGroupName] = componentGroupFields[componentName][componentGroupName].concat(flattenComponent(compName, dom));
             }
@@ -255,29 +252,29 @@ function dictionaryGroups(dom) {
 }
 
 function getFixVer(dom) {
-
-    var fixMaj = xpath.select("//fix/@major", dom)[0].value;
-    var fixMin = xpath.select("//fix/@minor", dom)[0].value;
-    var fixSp = xpath.select("//fix/@servicepack", dom)[0].value;
+    const fixMaj = xpath.select("//fix/@major", dom)[0].value;
+    const fixMin = xpath.select("//fix/@minor", dom)[0].value;
+    const fixSp = xpath.select("//fix/@servicepack", dom)[0].value;
     FIX_VER = [fixMaj, fixMin, fixSp].join('.');
-
 }
 
 function readDataDictionary(fileLocation) {
 
-    var xml = fs.readFileSync(fileLocation).toString();
-    var dom = new DOMParser().parseFromString(xml);
-    var nodes = xpath.select("//fix/fields/field", dom);
+    // TODO: lazy load data dictionary based upon message type coming in
+    
+    const xml = fs.readFileSync(fileLocation).toString();
+    const dom = new DOMParser().parseFromString(xml);
+    const nodes = xpath.select("//fix/fields/field", dom);
 
     getFixVer(dom);
 
-    for (var i = 0; i < nodes.length; i++) {
-        var tagNumber = nodes[i].attributes[0].value;
-        var tagName = nodes[i].attributes[1].value;
-        var tagType = nodes[i].attributes[2].value;
-        var valElem = nodes[i].getElementsByTagName('value');
-        var values = {};
-        for (var j = 0; j < valElem.length; j++) {
+    for (let i = 0; i < nodes.length; i++) {
+        const tagNumber = nodes[i].attributes[0].value;
+        const tagName = nodes[i].attributes[1].value;
+        const tagType = nodes[i].attributes[2].value;
+        const valElem = nodes[i].getElementsByTagName('value');
+        let values = {};
+        for (let j = 0; j < valElem.length; j++) {
             values[valElem[j].attributes[0].value] = valElem[j].attributes[1].value.replace(/_/g, ' ');
         }
         TAGS[tagNumber] = {
